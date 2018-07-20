5747e5a19fa859a0ead749de09a62e33246364bb
Anirvan Roy <anirvanroy1986@gmail.com>
2018-07-19 21:44:47 -0400

    Initial commit for the Replenisher task API

---
+++ src/main/java/com/anirvan/replenisher/ReplenisherTaskApiApplication.java
@@ -0,0 +1,12 @@
+package com.anirvan.replenisher.replenishertaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplenisherTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplenisherTaskApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/anirvan/replenisher/replenishertaskapi/ReplenisherTaskApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anirvan.replenisher.replenishertaskapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReplenisherTaskApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
