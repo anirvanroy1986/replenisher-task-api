de6aff59476ef313a802dbd36061e2d8413433d2
Anirvan Roy <anirvanroy1986@gmail.com>
2018-07-21 15:42:15 -0400

    Fix issues related to the Gradle project setup

--- src/main/java/com/anirvan/replenisher/ReplenisherTaskApiApplication.java
+++ src/main/java/com/anirvan/replenisher/ReplenisherTaskApiApplication.java
@@ -1,4 +1,4 @@
-package com.anirvan.replenisher.replenishertaskapi;
+package com.anirvan.replenisher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
